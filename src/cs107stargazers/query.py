52544d197164a6cf369f5caf0cdfed0631083320
inm560 <innocentmunai@college.harvard.edu>
2023-12-17 14:47:09 -0500

    #docs: Add documentation to the `Query` class

--- src/cs107stargazers/query.py
+++ src/cs107stargazers/query.py
@@ -50,54 +50,117 @@ Dependencies:
 """
 
 
-import pandas as pd 
+import pandas as pd
 from astroquery.sdss import SDSS
 
-#alright create the query class 
-class Query(): 
 
-    def __init__(self, ADQL_string : str): 
+# alright create the query class
+class Query:
+    """
+    A class for querying the Sloan Digital Sky Survey (SDSS) database using ADQL (Astronomical Data Query Language).
+
+    This class provides methods to execute ADQL queries against the SDSS database and retrieve results
+    in different formats such as pandas DataFrame, Astropy Table, or simply print the results.
+
+    Attributes:
+    -----------
+    ADQL_string : str
+        The ADQL query string to be executed against the SDSS database.
+
+    Methods:
+    --------
+    get_df():
+        Executes the ADQL query and returns the result as a pandas DataFrame.
+
+    get_table():
+        Executes the ADQL query and returns the result as an Astropy Table.
+
+    print():
+        Executes the ADQL query and prints the result.
+    """
+
+    def __init__(self, ADQL_string: str):
+        """
+        Initializes the Query object with the given ADQL query string.
+
+        Parameters:
+        -----------
+        ADQL_string : str
+            The ADQL query string to be used for querying the SDSS database.
+        """
         self.ADQL_string = ADQL_string
         try:
             from astroquery.sdss import SDSS
             import pandas as pd
+
             self.SDSS = SDSS
             self.pandas = pd
         except ImportError:
             print("Please install the required libraries using:")
             print("pip install astroquery pandas")
             # You may exit the program or take appropriate action based on your use case
-            exit(1)        
+            exit(1)
 
-    
-    def get_df(self): 
+    def get_df(self):
+        """
+        Executes the stored ADQL query and returns the results as a pandas DataFrame.
+
+        Returns:
+        --------
+        pandas.DataFrame
+            A DataFrame containing the query results.
+
+        Raises:
+        -------
+        Exception
+            If an error occurs during the query execution or data conversion.
+        """
         try:
             # Query SDSS using the provided ADQL query
-            result = SDSS.query_sql(self.ADQL_string)           
+            result = SDSS.query_sql(self.ADQL_string)
             df = result.to_pandas()
             return df
-        
+
         except Exception as e:
             print(f"An error occurred. Make sure you have a valid ADQL query.")
 
-    def get_table(self): 
+    def get_table(self):
+        """
+        Executes the stored ADQL query and returns the results as an Astropy Table.
+
+        Returns:
+        --------
+        astropy.table.Table
+            An Astropy Table containing the query results.
+
+        Raises:
+        -------
+        Exception
+            If an error occurs during the query execution.
+        """
         try:
             # Query SDSS using the provided ADQL query
-            result = SDSS.query_sql(self.ADQL_string)          
+            result = SDSS.query_sql(self.ADQL_string)
             return result
-        
+
         except Exception as e:
             print(f"An error occurred. Make sure you have a valid ADQL query.")
 
-    def print(self): 
+    def print(self):
+        """
+        Executes the stored ADQL query and prints the results.
+
+        This method is useful for quick checks or debugging purposes.
+
+        Raises:
+        -------
+        Exception
+            If an error occurs during the query execution.
+        """
         try:
             # Query SDSS using the provided ADQL query
-            result = SDSS.query_sql(self.ADQL_string)          
+            result = SDSS.query_sql(self.ADQL_string)
             print(result)
-        
+
         except Exception as e:
             print(f"An error occurred. Make sure you have a valid ADQL query.")
-
-
-
-
