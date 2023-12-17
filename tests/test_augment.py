6b8aad5b2ddb93ecb660f2a0057ed877ba5e4c48
Elena Jauregui <ejauregui@hsph.harvard.edu>
2023-12-17 11:39:30 -0500

    Update test_augment.py
    Deleting duplicated code, was causing bugs in Actions/CI

--- tests/test_augment.py
+++ tests/test_augment.py
@@ -9,15 +9,6 @@ from _pytest.monkeypatch import MonkeyPatch
 import sys
 import os
 
-
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
-)
-from augment import Augment 
-
-import sys
-import os
-
 sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
 )
